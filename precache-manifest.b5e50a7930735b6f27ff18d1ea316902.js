76faba1a34250951fc9edbb3ad3a762f8be7580f
Vasileios Psychas <vaspsych@gmail.com>
2019-10-29 16:20:01 +0100

    Updates

---
+++ precache-manifest.b5e50a7930735b6f27ff18d1ea316902.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d19603ff3e6431b33a712d71a0fdec0c",
+    "url": "/SPA-todo-app/index.html"
+  },
+  {
+    "revision": "807c2aeea71a0d5257b8",
+    "url": "/SPA-todo-app/static/css/main.fcb06fc8.chunk.css"
+  },
+  {
+    "revision": "29a1ad81a791b84f6e93",
+    "url": "/SPA-todo-app/static/js/2.107bf40b.chunk.js"
+  },
+  {
+    "revision": "807c2aeea71a0d5257b8",
+    "url": "/SPA-todo-app/static/js/main.81c8dcd9.chunk.js"
+  },
+  {
+    "revision": "9e889fc373b88b272d9b",
+    "url": "/SPA-todo-app/static/js/runtime-main.a68b6d18.js"
+  }
+]);
