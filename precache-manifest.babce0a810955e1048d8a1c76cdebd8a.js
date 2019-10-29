05b659fa29b2863b82ed8a0ec53d2171bcb2ea52
Vasileios Psychas <vaspsych@gmail.com>
2019-10-29 16:35:08 +0100

    Updates

---
+++ precache-manifest.babce0a810955e1048d8a1c76cdebd8a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07026b4dbde7f72891f81dc3b352e957",
+    "url": "/SPA-todo-app/index.html"
+  },
+  {
+    "revision": "02a33b797a7b66fe0656",
+    "url": "/SPA-todo-app/static/css/main.fcb06fc8.chunk.css"
+  },
+  {
+    "revision": "29a1ad81a791b84f6e93",
+    "url": "/SPA-todo-app/static/js/2.107bf40b.chunk.js"
+  },
+  {
+    "revision": "02a33b797a7b66fe0656",
+    "url": "/SPA-todo-app/static/js/main.9789d6ab.chunk.js"
+  },
+  {
+    "revision": "9e889fc373b88b272d9b",
+    "url": "/SPA-todo-app/static/js/runtime-main.a68b6d18.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.1f2ac72ea3fbec9af9967ce14ee390f5.js"
+  "/SPA-todo-app/precache-manifest.babce0a810955e1048d8a1c76cdebd8a.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/SPA-todo-app/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
