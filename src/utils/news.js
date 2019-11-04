27be21bb5292e5b4b5bf28a9c8fc98184a9595e1
Vasileios Psychas <vaspsych@gmail.com>
2019-11-04 22:06:30 +0100

    Oops

--- src/utils/news.js
+++ src/utils/news.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const apiKey = `f8d7869212c24907bc585db6b6d267be`;
+const apiKey = `YOUR API KEY`;
 export default axios.create({
   baseURL: `https://newsapi.org/v2/top-headlines?sources=bbc-news&apiKey=${apiKey}`
 });
